582e13a17242336a37b587631b0663d99b65f6a8
Tobias Marquardt <tm@tobix.eu>
2014-09-25 17:06:30 +0200

    Added xmonad configuration.

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,35 @@
+import XMonad
+import XMonad.Util.EZConfig
+import qualified XMonad.StackSet as W
+
+myTerminal      = "konsole"
+myBorderWidth   = 3
+myModMask       = mod4Mask
+myFocusedBorderColor = "#0000ff"
+myStartupHook :: X ()
+myStartupHook = spawn "setxkbmap de"
+myManageHook :: [ManageHook]
+myManageHook =
+    [ className =? "Yakuake" --> doFloat
+    , className =? "Pidgin" --> doFloat
+    ]
+myKeyBindings =
+-- Move mod-h/j/k/l bindings to mod-j/k/l/ö
+    [ ((myModMask, xK_j        ), sendMessage Shrink )
+    , ((myModMask, xK_k        ), windows W.focusDown)
+    , ((myModMask, xK_l        ), windows W.focusUp  )
+    , ((myModMask, 0xf6        ), sendMessage Expand )
+    , ((myModMask .|. shiftMask, xK_k        ), windows W.swapDown )
+    , ((myModMask .|. shiftMask, xK_l        ), windows W.swapUp )
+    ]
+
+-- defaults are defined in xmonad/XMonad/Config.hs
+main = xmonad $ defaultConfig {
+    terminal             = myTerminal
+    , borderWidth        = myBorderWidth
+    , modMask            = myModMask
+    , focusedBorderColor = myFocusedBorderColor
+    , startupHook        = myStartupHook
+    , manageHook = manageHook defaultConfig <+> composeAll myManageHook
+    }
+    `additionalKeys` myKeyBindings
